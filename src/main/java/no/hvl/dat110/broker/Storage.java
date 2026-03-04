99daaf3a165c69d09be614b89aa34fc98d1fcd24
186485 <kasper.austboe@gmail.com>
2026-03-04 13:35:56 +0100

    Gjort B1

--- src/main/java/no/hvl/dat110/broker/Storage.java
+++ src/main/java/no/hvl/dat110/broker/Storage.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import no.hvl.dat110.client.Client;
 import no.hvl.dat110.common.TODO;
 import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.messagetransport.Connection;
@@ -51,51 +52,30 @@ public class Storage {
 	}
 
 	public void addClientSession(String user, Connection connection) {
-
-		// TODO: add corresponding client session to the storage
-		// See ClientSession class
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+        ClientSession session = new ClientSession(user, connection);
+        clients.put(user, session);
 	}
 
 	public void removeClientSession(String user) {
-
-		// TODO: disconnet the client (user) 
-		// and remove client session for user from the storage
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+        ClientSession session = clients.get(user);
+        clients.remove(user);
 	}
 
 	public void createTopic(String topic) {
-
-		// TODO: create topic in the storage
-
-		throw new UnsupportedOperationException(TODO.method());
-	
+       subscriptions.putIfAbsent(topic,  ConcurrentHashMap.newKeySet());
 	}
 
 	public void deleteTopic(String topic) {
-
-		// TODO: delete topic from the storage
-
-		throw new UnsupportedOperationException(TODO.method());
-		
+        subscriptions.remove(topic);
 	}
 
 	public void addSubscriber(String user, String topic) {
 
-		// TODO: add the user as subscriber to the topic
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+    subscriptions.putIfAbsent(user, ConcurrentHashMap.newKeySet());
+    subscriptions.get(topic).add(user);
 	}
 
 	public void removeSubscriber(String user, String topic) {
-
-		// TODO: remove the user as subscriber to the topic
-
-		throw new UnsupportedOperationException(TODO.method());
+        subscriptions.get(topic).remove(user);
 	}
 }
