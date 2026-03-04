18e9555b41268332022cb7ae3872d7e572044ce2
186485 <kasper.austboe@gmail.com>
2026-03-04 13:11:06 +0100

    Laget kronstruktør osv

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -3,14 +3,38 @@ package no.hvl.dat110.messages;
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
-
+    private String topic;
+    private String user;
     public CreateTopicMsg(String user, String topic) {
-
+        this.topic = topic;
+        this.user = user;
     }
+
+    public String getTopic() {
+        return topic;
+    }
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String toString(){
+       String str = "topic: "+topic+"\nuser: "+user;
+       return str;
+    }
+
+
+
 
 	// TODO: 
 	// Implement object variables - a topic is required
-	
+
+
+
 	// Complete the constructor, get/set-methods, and toString method
     // as described in the project text	
 }
